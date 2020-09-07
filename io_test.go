d60488dcdeacb03e9d11cc519ed8c811d37574bb
Manfred Touron <94029+moul@users.noreply.github.com>
2020-09-07 17:03:55 +0200

    feat: add io stream capture helper

--- io_test.go
+++ io_test.go
@@ -1,6 +1,7 @@
 package u_test
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -29,4 +30,118 @@ func TestSilentClose(t *testing.T) {
 func ExampleSilentClose() {
 	f, _ := os.Open("file.txt")
 	defer u.SilentClose(f)
+}
+
+func ExampleCaptureStdout() {
+	fmt.Println("AAA")
+	closer, err := u.CaptureStdout()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("BBB")
+	ret := closer()
+	fmt.Println("CCC")
+	fmt.Println(ret)
+	// Output:
+	// AAA
+	// CCC
+	// BBB
+}
+
+func ExampleMustCaptureStdout() {
+	fmt.Println("AAA")
+	closer := u.MustCaptureStdout()
+	fmt.Println("BBB")
+	ret := closer()
+	fmt.Println("CCC")
+	fmt.Println(ret)
+	// Output:
+	// AAA
+	// CCC
+	// BBB
+}
+
+func ExampleCaptureStderr() {
+	os.Stderr = os.Stdout // hack to run this test as an example test
+
+	fmt.Fprintln(os.Stderr, "AAA")
+	closer, err := u.CaptureStderr()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(os.Stderr, "BBB")
+	ret := closer()
+	fmt.Fprintln(os.Stderr, "CCC")
+	fmt.Fprintln(os.Stderr, ret)
+	// Output:
+	// AAA
+	// CCC
+	// BBB
+}
+
+func ExampleMustCaptureStderr() {
+	os.Stderr = os.Stdout // hack to run this test as an example test
+
+	fmt.Fprintln(os.Stderr, "AAA")
+	closer := u.MustCaptureStderr()
+	fmt.Fprintln(os.Stderr, "BBB")
+	ret := closer()
+	fmt.Fprintln(os.Stderr, "CCC")
+	fmt.Fprintln(os.Stderr, ret)
+	// Output:
+	// AAA
+	// CCC
+	// BBB
+}
+
+func ExampleCaptureStdoutAndStderr() {
+	os.Stderr = os.Stdout // hack to run this test as an example test
+
+	fmt.Fprintln(os.Stderr, "AAA")
+	fmt.Println("BBB")
+	closer, err := u.CaptureStdoutAndStderr()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(os.Stderr, "CCC")
+	fmt.Println("DDD")
+	ret := closer()
+	fmt.Fprintln(os.Stderr, "EEE")
+	fmt.Println("FFF")
+	fmt.Println(ret)
+	// Output:
+	// AAA
+	// BBB
+	// EEE
+	// FFF
+	// CCC
+	// DDD
+}
+
+func ExampleMustCaptureStdoutAndStderr() {
+	os.Stderr = os.Stdout // hack to run this test as an example test
+
+	fmt.Fprintln(os.Stderr, "AAA")
+	fmt.Println("BBB")
+	closer := u.MustCaptureStdoutAndStderr()
+	fmt.Fprintln(os.Stderr, "CCC")
+	fmt.Println("DDD")
+	ret := closer()
+	fmt.Fprintln(os.Stderr, "EEE")
+	fmt.Println("FFF")
+	fmt.Println(ret)
+	// Output:
+	// AAA
+	// BBB
+	// EEE
+	// FFF
+	// CCC
+	// DDD
+}
+
+func TestCaptureStderr(t *testing.T) {
+	closer := u.MustCaptureStderr()
+	fmt.Fprintln(os.Stderr, "hello world")
+	ret := closer()
+	require.Equal(t, ret, "hello world\n")
 }
