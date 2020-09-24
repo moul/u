8e39e9df632063c67453ca8abf2374f0ba4b6e2a
Manfred Touron <94029+moul@users.noreply.github.com>
2020-09-24 22:55:28 +0200

    feat: func {Path,Dir,File}Exists(path string)

--- os_test.go
+++ os_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 
 	"moul.io/u"
 )
@@ -58,4 +59,49 @@ func ExampleMustExpandUser() {
 	ret := u.MustExpandUser("~/hello-world.txt")
 	fmt.Println(ret)
 	// Output: /home/foo/hello-world.txt
+}
+
+func ExamplePathExists() {
+	file, err := ioutil.TempFile("", "bar")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(file.Name())
+	fmt.Println(u.PathExists("/laksjdflkasdjflaksdjfalskdfjasdlfkj")) // should not exist
+	fmt.Println(u.PathExists(file.Name()))
+	fmt.Println(u.PathExists(filepath.Dir(file.Name())))
+	// Output:
+	// false
+	// true
+	// true
+}
+
+func ExampleDirExists() {
+	file, err := ioutil.TempFile("", "bar")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(file.Name())
+	fmt.Println(u.DirExists("/laksjdflkasdjflaksdjfalskdfjasdlfkj")) // should not exist
+	fmt.Println(u.DirExists(file.Name()))
+	fmt.Println(u.DirExists(filepath.Dir(file.Name())))
+	// Output:
+	// false
+	// false
+	// true
+}
+
+func ExampleFileExists() {
+	file, err := ioutil.TempFile("", "bar")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(file.Name())
+	fmt.Println(u.FileExists("/laksjdflkasdjflaksdjfalskdfjasdlfkj")) // should not exist
+	fmt.Println(u.FileExists(file.Name()))
+	fmt.Println(u.FileExists(filepath.Dir(file.Name())))
+	// Output:
+	// false
+	// true
+	// false
 }
