8cb7ee7613b5b68e8800ccd50b744b619270bff3
Manfred Touron <94029+moul@users.noreply.github.com>
2020-12-29 13:46:06 +0100

    feat: add Future

--- pattern_test.go
+++ pattern_test.go
@@ -3,6 +3,7 @@ package u_test
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"moul.io/u"
 )
@@ -18,4 +19,21 @@ func ExampleCombineFuncs() {
 func ExampleCheckErr() {
 	_, err := http.Get("http://foo.bar")
 	u.CheckErr(err) // panic
+}
+
+func ExampleFuture() {
+	future := u.Future(func() (interface{}, error) {
+		time.Sleep(100 * time.Millisecond)
+		return "foobar", nil
+	})
+
+	// here, we can do some stuff
+
+	ret := <-future
+	fmt.Println("Ret:", ret.Ret)
+	fmt.Println("Err:", ret.Err)
+
+	// Output:
+	// Ret: foobar
+	// Err: <nil>
 }
